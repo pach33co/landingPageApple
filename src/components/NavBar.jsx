f1aa7518181b187d352e34839d8f964708329726
Matheus Pacheco <pachecmm@gmail.com>
2026-07-23 20:49:14 -0300

    refactor: reorganizar a organização do arquivo da NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,23 @@
 function NavBar() {
     return (
         <nav className="fixed top-0 w-full bg-black/80 backdrop-blur-md z-50">
-            <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-center gap-8">
-                <a href="#design"className="hover:text-gray-300">Design</a>
-                <a href="#cameras"className="hover:text-gray-300">Câmeras</a>
-                <a href="#desempenho"className="hover:text-gray-300">Desempenho</a>
-                <a href="#acessorios"className="hover:text-gray-300">Acessórios</a>
-                <button className="bg-blue-700 px-6 py-2 rounded-full">Comprar</button>
+            <div className="flex items-center justify-between px-6 py-4 w-full max-w-7xl mx-auto">
+
+                <div className="-m-1.5 p-1.5">
+                    <img className="h-8 w-auto" src="../src/assets/LogoAppleNavBar.svg" />
+                </div>
+
+                <div className="flex-1 flex items-center justify-center gap-8">
+                    <a href="#design" className="hover:text-gray-300">Design</a>
+                    <a href="#cameras" className="hover:text-gray-300">Câmeras</a>
+                    <a href="#desempenho" className="hover:text-gray-300">Desempenho</a>
+                    <a href="#acessorios" className="hover:text-gray-300">Acessórios</a>
+                </div>
+
+                <div>
+                    <button className="bg-blue-700 px-6 py-1.5 rounded-full">Comprar</button>
+                </div>
+
             </div>
         </nav>
     )
